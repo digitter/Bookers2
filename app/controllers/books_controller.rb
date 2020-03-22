1d99da1e35b1ec0459e6f85ebd8c2257e44b852f
kazuki <matchingserver@gmail.com>
2020-03-22 16:58:51 +0900

    finish boook exept index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,56 @@
 class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+    if user_signed_in?
+      @book = Book.find(params[:id])
+      @user = User.find(@book.user_id)
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    if book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(book.id)
+    else
+      @user = User.find(current_user.id)
+      @book = book
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def edit
+    if user_signed_in?
+      @book = Book.find(params[:id])
+    else
+      redirect_to new_user_sessio_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'You have updated book successfully.'
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 end
