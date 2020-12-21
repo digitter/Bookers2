990b1916cc7892c84d30a6578e59d2d8b25b5091
kazuki <matchingserver@gmail.com>
2020-12-22 02:44:51 +0900

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1000.times do |i|
+  u = User.new(name: "user#{i}", introduction: "introduction#{i}", email: "#{i}@#{i}.#[i}", password: "#{i}" * 6, password_confirmation: "#{i}" * 6)
+  u.save!
+  Book.create!(user_id: u.id, title: "#{i}", body: "body body body")
+end
+
